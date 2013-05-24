a16499f4dad9319a30eab73b7948d2be990083b7
Michael Hnatiw <hnatiwm4@gmail.com>
2013-05-24 02:02:43 -0400

    installed npm and nodejs correclt and dependencies, must test

--- application/views/template.php
+++ application/views/template.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
 <head>
+
    <meta http-equiv="Content-Style-Type" content="text/html; charset=utf-8"/>
    <title><?php echo html::specialchars($title) ?></title>
-   <!-- link for tab icon -->
+
+   <!-- LINKS -->
+   
+   <!-- link for boostrap.less to compile less files -->
+   <link rel="stylesheet/less" href="/mediaflow/media/less/bootstrap.less" />
+   <!-- link for browser tab icon -->
    <link rel="shortcut icon" href="/mediaflow/media/ico/thumb1.ico" />
+
    <!-- Cascading stylesheets -->
    <?php echo html::stylesheet(
          array('media/css/site',),
          array('screen',)
      ); 
    ?>
+
+   <!-- JAVASCRIPTS -->
+
+   <!-- link to include javascripts for LESS Module -->
+   <script src="/mediaflow/media/js/less-1.3.3.min.js" ></script>
+
 </head>
 
 <body background="<?php echo $bg ?>" >
