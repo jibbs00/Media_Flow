b7a47490d364051d02db93709e7b1ba7f230ffca
Michael Hnatiw <hnatiwm4@gmail.com>
2013-05-27 02:55:17 -0400

    conctructing home

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -3,13 +3,13 @@
 </div>
 
 <div id="container">
-     <div id="center" class="column">
+     <div id="center" class="column outter-box-shading">
      	  <p>Are you a <em>Lamer</em>? are you a <em>Babe Banisher</em>?</p>
      </div>
-     <div id="left" class="column left-inner-shading">
+     <div id="left" class="column leftcol-inner-shading">
 	   <p>Get L33t today!</p>
      </div>
-     <div id="right" class="column">
+     <div id="right" class="column rightcol-inner-shading">
      	  <h3>Add Content!</h3>
      	  <input class="input-rightcol" type="text" placeholder="type in a URL">
      </div>
