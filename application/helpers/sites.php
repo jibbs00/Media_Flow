de458d0a8658e9b413aae01dee01ac62fc52313f
Michael Hnatiw <hnatiwm4@gmail.com>
2013-06-10 12:09:26 -0400

    overwrite $content variable in controller to dynamically change html

--- application/helpers/sites.php
+++ application/helpers/sites.php
@@ -7,35 +7,40 @@
 
 class sites_core
 {
-    public static function add_url($user)
+   /*
+     function using PHP moule DomDocument to re-write the
+     HTML of a designated webpage, adding URL links
+     by (either from the database or inputted by the user)
+     
+     @param $content - 'View' Object for page
+     @param $view - View being dynamically re-written or modified
+     @param $element - html element being added or modified 
+     @return $content 
+   */
+    public static function add_url($content)
     {
-        //variable for main view filename
-        $home = '/var/www/html/mediaflow/application/views/pages/home.php';
-    
-        // open temporary file for writing a new dynaic view
-	//$file = fopen('temp_home.php','w');
-	//fwrite($file,'d');
-	//fclose($file);
+        // Variable for main view filename
+        $view = '/var/www/html/mediaflow/application/views/pages/home.php';
 
         // Load the document
-	$doc = new DOMDocument;
-	
-	$doc->loadHTMLFile($home);
+	$doc = new DOMDocument;	
+	$doc->loadHTMLFile($view);
 
-	//determine place to insert code
-	$parent = $doc->getElementsByTagName('form')->item(0);
+	// Determine place to insert code
+	//$parent = $doc->getElementsByTagName('form')->item(0);
+	$parent = $doc->getElementById('right');
 
 	// Create the child element 
-	$child = $doc->createElement('a');
+	$child = $doc->createElement('a','Google');
 	$child->setAttribute('href', 'https://www.google.ca');
-	$child->setAttribute('value','Google');
 
 	// Append (insert) the child to the parent node
 	$parent->appendChild($child);
 
 	// Save the appeneded file
-	echo $doc->saveHTMLFile($home);
-	
+	//echo $doc->saveHTML();
+
+	return $doc->saveHTML();
     }
 	
 }
