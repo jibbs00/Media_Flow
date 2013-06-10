cff22ba24977d828d4a16aac00871d81027057e0
Michael Hnatiw <hnatiwm4@gmail.com>
2013-06-10 12:22:07 -0400

    to dynamically change HTML, reset $content for a controller

--- application/helpers/sites.php
+++ application/helpers/sites.php
@@ -7,34 +7,39 @@
 
 class sites_core
 {
+    /*
+     function using PHP module DomDocument to reqrite the HTML of a
+     designated webpage, adding URL links by either from the database
+     or inputted by the user
+
+     @param $content - 'View' object for page
+     @param $view - View being dynamicallt re-written or modified
+     @param $element - HTML element being added or modified
+     @return $content
+
+    */
     public static function add_url($user)
     {
-        //variable for main view filename
-        $home = '/var/www/html/mediaflow/application/views/pages/home.php';
-    
-        // open temporary file for writing a new dynaic view
-	//$file = fopen('temp_home.php','w');
-	//fwrite($file,'d');
-	//fclose($file);
+        // Variable for main view filename
+        $view = '/var/www/html/mediaflow/application/views/pages/home.php';
 
         // Load the document
-	$doc = new DOMDocument;
-	
-	$doc->loadHTMLFile($home);
+	$doc = new DOMDocument;	
+	$doc->loadHTMLFile($view);
 
 	//determine place to insert code
-	$parent = $doc->getElementsByTagName('form')->item(0);
+	//$parent = $doc->getElementsByTagName('form')->item(0);
+	$parent = $doc->getElementById('right');
 
 	// Create the child element 
-	$child = $doc->createElement('a');
+	$child = $doc->createElement('a','Google');
 	$child->setAttribute('href', 'https://www.google.ca');
-	$child->setAttribute('value','Google');
 
 	// Append (insert) the child to the parent node
 	$parent->appendChild($child);
 
 	// Save the appeneded file
-	echo $doc->saveHTMLFile($home);
+	return $doc->saveHTML();
 	
     }
 	
